1f4e732776021718b1346251ecb79f3b23dba7e5
wpinocho <julian.ruiz.loza@gmail.com>
2025-08-29 16:12:23 -0600

    AI Update: 3 files updated by Lovivo

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Index from "./pages/Index";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
+        <Route path="/index" element={<Index />} />
         <Route path="/index" element={<Index />} />
         </Routes>
       </BrowserRouter>
