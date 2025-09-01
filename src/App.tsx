47bf21bfe20c3a2621ca354a6d401a0c1619fab5
wpinocho <julian.ruiz.loza@gmail.com>
2025-09-01 10:22:03 -0600

    AI Update: 3 files updated by Lovivo

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Index from "./pages/Index";
 import Index from "./pages/Index";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
+        <Route path="/index" element={<Index />} />
         <Route path="/index" element={<Index />} />
         <Route path="/index" element={<Index />} />
         </Routes>
